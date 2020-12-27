e86c6ed3db69e700d8120eb04fa06b8e6bd8e923
dudeimconfused <74417622+dudeimconfused@users.noreply.github.com>
2020-12-27 19:33:27 +0530

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,19 @@
+﻿{
+	"version": 1609077179,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/captchaframe-sheet0.png",
+		"images/captcha-sheet0.png",
+		"images/captcha-sheet1.png",
+		"images/captcha-sheet2.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
